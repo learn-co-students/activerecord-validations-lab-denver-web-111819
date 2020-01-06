6d5966ed4438835c574173be82ea66c1145673fd
alexiscait142 <chilinski.a@gmail.com>
2020-01-06 14:44:04 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 250}
+    validates :summary, presence: true, length: {maximum: 250}
+    validates :category, presence: true, inclusion: {in: %w{Fiction Non-Fiction}}
+    validates_with ClickbaitValidator
+
 end
+
+# validates :title, clickbait: true
