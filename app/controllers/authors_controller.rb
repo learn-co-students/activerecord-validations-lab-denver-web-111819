06f7c586358ffbfbbcb519e6b5a4c65ed46914a3
github username <github email address>
2020-01-06 14:44:36 -0700

    completed first 7 tasks, working on final task

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,19 @@
+class AuthorsController < ApplicationController
+    # def index 
+    #     @authors = Author.all 
+    #     render json: @authors 
+    # end 
+    # def create
+    #     @author = Author.new(author_params)
+    #     if @author.valid? 
+    #         @author.save 
+    #         render json: @author 
+    #     end 
+    # end 
+    # private 
+
+    # def author_params 
+    #     params.require(:author).permit(:name, :phone_number)
+    # end 
+        
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true, uniqueness: true
+    # validate :title, if: :clickbait
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    # def clickbait 
+    #     title.include?("Won't Believe", "Secret", "Top 5", "Guess")
+    # end 
+    
+
 end
